f3832b2f0121bc35407a6221792ee76c1e7315b3
joao0cb <joao.00000853774@unicap.br>
2024-10-23 21:56:10 +0000

    Lista Struct

--- Lista Struct/Q2.c
+++ Lista Struct/Q2.c
@@ -14,8 +14,30 @@ struct Papelaria {
 int main() {
     struct Papelaria cadastro[40];
     printf("O que você deseja fazer?\n");
-    printf("1 - Cadastro de novo produto.\n");
-    printf("Alterar valor de um produto.\n");
-    printf("Valor de um produto.\n");
-    printf("")
+    printf("1 - Cadastrar produto.\n");
+    printf("2 - Alterar valor.\n");
+    printf("3 - Valor de um produto.\n");
+    printf("4 - Estoque de um produto.\n");
+    printf("5 - Venda de um produto.\n");
+    printf("6 - Atualizar estoque.\n");
+    printf("Exibir produtos.\n");
+    printf("Exibir produtos com estoque 0.\n");
 }
+
+/*
+int Menu() {
+    int escolha = 0;
+    do {
+        printf("(1)Cliente\n(2)Funcionário\n");
+        scanf("%d", &escolha);
+        switch (escolha)
+        {
+        case 1:
+            printf("O que você deseja fazer?\n");
+            printf("Valor de um produto.\n");
+            printf("Estoque de um produto.\n");
+        }
+    
+
+}
+*/
